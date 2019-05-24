0ae7d4ee2dc12bfb9b93239eb4d6f0aa3752c338
songshuran <happy_ran@outlook.com>
2019-05-24 17:36:29 +0800

    struct 序列化

---
+++ day04/demo02/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Monster struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Skill string `json:"skill"`
+}
+
+func main() {
+	//结构体字段上添加标签
+	monster := Monster{"牛魔王", 168, "芭蕉扇"}
+
+	//序列化结构体为json字符串
+	jsonByte, _ := json.Marshal(monster)
+	fmt.Println(string(jsonByte))
+}
