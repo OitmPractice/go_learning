0b166419d74e6629f9b315c85f514434b8fed6ce
songshuran <happy_ran@outlook.com>
2019-06-12 16:57:50 +0800

    ignore

---
+++ day07/demo01/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "errors"
+
+type Queue struct {
+	maxSize int
+	array   [4]int
+	front   int
+	rear    int
+}
+
+func (this *Queue) AddQueue(val int) (err error) {
+	if this.rear == this.maxSize-1 {
+		return errors.New("queue full")
+	}
+	this.rear++
+	this.array[this.rear] = val
+
+	return
+}
+
+func (this *Queue) ShowQueue() {
+
+}
+
+func main() {
+
+}
